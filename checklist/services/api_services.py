c9a4602baa640df9f303bfb674c8be0d90efa25a
Deiner Rodrigues de Souza <deiner.souza@win.ufu.br>
2026-01-21 21:55:37 -0300

    ajuste no envio de ordens de serviço, pois a API estava com um erro lógico enviando somente a ultima ordem da lista.

--- checklist/services/api_services.py
+++ checklist/services/api_services.py
@@ -2,18 +2,21 @@ from checklist.models import *
 
 def get_pending_work_order():
     items = WorkOrder.objects.all()
+    data = []
     for item in items:
         client = Client.objects.get(id=item.client.id)
         if item.status !=  "4":
-            data = [
+            print(f"work_order : {item}")
+            data.append(
                 {
                 "operation_code" : item.operation_code,
                 "symptoms" : item.symptoms,
                 "client" : client.name,
                 "status" : item.status,
                 "insert_date" :item.insert_date,
-                }            
-            ]
+                })
+            print(f"Data : {data}")            
+            
     return data
 
 def get_checklist_items():
