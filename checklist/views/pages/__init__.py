bab3343167b63b434231429841f3d11e6fb328ac
Deiner Rodrigues de Souza <deinersouza.tmsi@gmail.com>
2026-04-08 21:25:21 -0300

    chore(database): insert test data for form validation

---
+++ checklist/views/pages/__init__.py
@@ -0,0 +1,6 @@
+from .client_pages import *
+from .address_pages import *
+from .checklist_item_pages import *
+from .employe_pages import *
+from .service_order_pages import *
+from .core_pages import *
